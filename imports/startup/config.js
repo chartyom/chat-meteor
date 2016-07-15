70d153e68c670bddc9a9f8e01ca7f946e4321c92
Артём <leo240532@yandex.ru>
2016-07-15 21:50:43 +0300

    update 0.0.6 design and js

--- imports/startup/config.js
+++ imports/startup/config.js
@@ -1,18 +1,4 @@
-const Options = {
+export const Options = {
     siteName: 'App',
     separatorSiteName: ' - '
-};
-
-export const Page = {
-    setTitle: function(title){
-        document.title = (title !== undefined) ? Options.siteName +
-        Options.separatorSiteName +
-        title : Options.siteName;
-    },
-    setNewTitle: function(title){
-        document.title = title;
-    },
-    getTitle: function(){
-        return document.title;
-    },
 };
