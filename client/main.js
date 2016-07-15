428ae5a0f213cf06e18102d26400a252912a76c5
Артём <leo240532@yandex.ru>
2016-07-15 21:54:04 +0300

    update 0.0.6 design and js

--- client/main.js
+++ client/main.js
@@ -15,4 +15,4 @@ import '/imports/widgets/sendMessageModal/WidgetSendMessageModal.js';
 import '/imports/widgets/dialogs/WidgetDialogs.js';
 import '/imports/widgets/dialogsMessages/WidgetDialogsMessages.js';
 
-import '/imports/Routes.js';
+import '/imports/lib/Routes.js';
